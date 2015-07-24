57ac93141566f7b6696b4263b3b197954b153100
MatthewKwong <mkwong.sh@mysummitps.org>
2015-07-24 16:23:58 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+* Created with JavaScript Notes.
+* User: MatthewKwong
+* Date: 2015-07-21
+* Time: 07:08 PM
+* To change this template use Tools | Templates.
+*/
+define(function() {
+return {};
+});
